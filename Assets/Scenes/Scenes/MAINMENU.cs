dc01305c14e529f6874a927eb7f8c4406d8cbe7c
Khoa-thich-code <khoauranium@gmail.com>
2023-10-05 19:58:12 +0700

    lan3

---
+++ Assets/Scenes/Scenes/MAINMENU.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MAINMENU : MonoBehaviour
+{
+    public Rigidbody2D startButtonRigidbody;
+    public float initialForce = 10.0f; // Lực ban đầu
+    public float delayBeforeStart = 2.0f; // Thời gian chờ trước khi bắt đầu trò chơi
+
+    private bool gameStarted = false;
+
+    public void PlayGame()
+    {
+        if (!gameStarted)
+        {
+            StartCoroutine(ShootStartButtonAndStartGame());
+        }
+    }
+
+    private IEnumerator ShootStartButtonAndStartGame()
+    {
+        gameStarted = true;
+
+        // Tắt xác định va chạm của nút bắt đầu
+        startButtonRigidbody.collisionDetectionMode = CollisionDetectionMode2D.Discrete;
+
+        // Áp dụng lực ban đầu cho nút bắt đầu
+        startButtonRigidbody.velocity = Vector2.zero;
+        startButtonRigidbody.AddForce(Vector2.right * initialForce, ForceMode2D.Impulse);
+
+        // Chờ 2 giây trước khi bắt đầu trò chơi
+        yield return new WaitForSeconds(delayBeforeStart);
+
+        // Bắt đầu trò chơi
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scenes/Scenes/TimelineController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    private void Start()
+    {
+        // Tìm và gán Playable Director trong scene (bằng cách kéo thả GameObject chứa Playable Director vào trường này trong Inspector).
+        playableDirector = FindObjectOfType<PlayableDirector>();
+        playableDirector.Stop(); // Dừng timeline khi khởi động scene.
+
+        // Đảm bảo bạn đã cài đặt "Loop" trong Inspector của Timeline nếu bạn muốn nó chạy liên tục.
+    }
+
+    public void PlayTimeline()
+    {
+        // Bắt đầu timeline.
+        playableDirector.Play();
+    }
+}
