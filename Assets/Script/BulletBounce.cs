33977ade466c625a80bb44d89fee26b2b1827ec0
thanhnld0912 <joshlemuel241291@gmail.com>
2023-10-12 19:56:57 +0700

    ko

--- Assets/Script/BulletBounce.cs
+++ Assets/Script/BulletBounce.cs
@@ -6,21 +6,21 @@ public class BulletBounce : MonoBehaviour
 {
     [SerializeField] private Transform firePosition;
     private Rigidbody2D rb;
-    Vector3 lastVelocity;
+    Vector2 lastVelocity;
+    Vector2 stopped = new Vector2(0, 0);
     public int  bounceCount = 5;
-    public float bulletSpeed = 2000.0f;
+
     private bool hit;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-       
     }
-
     private void Update()
     {
         lastVelocity = rb.velocity;
     }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var speed = lastVelocity.magnitude;
